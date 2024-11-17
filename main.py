357819a1c4cf0fd5748252f9911cf281cc37c9df
Felipe Brunet <felipe.brunet@gmail.com>
2024-11-17 14:32:00 -0300

    person_set

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from secrets import token_bytes
+import base58
+
 print('hello')
+
+class Person:
+    def __init__(self, sex, age):
+        self.sex = sex
+        self.age = age
+        self.id = base58.b58encode(token_bytes(10)).decode('utf-8')
+
+
+p1 = Person('male', 25)
+
+print(p1.id)
+print(p1.sex)
+print(p1.age)
